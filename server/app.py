2b7267c6fefef7bc5b3d1ea66c5e0db874c8f592
+ <+>
2023-10-16 23:38:47 +0300

    Complete Assignment

--- server/app.py
+++ server/app.py
@@ -1,8 +1,45 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
+
+@app.route("/")
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+
+@app.route("/print/<parameter>")
+def print_string(parameter):
+    return parameter
+
+
+@app.route("/count/<int:parameter>")
+def count(parameter):
+    numbers = "\n".join(map(str, range(parameter + 1)))
+    return numbers
+
+
+@app.route("/math/<int:num1>/<operation>/<int:num2>")
+def math(num1, operation, num2):
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Division by zero is not allowed."
+        result = num1 / num2
+    elif operation == "modulo":
+        if num2 == 0:
+            return "Modulo by zero is not allowed."
+        result = num1 % num2
+    else:
+        return "Invalid operation"
+
+    return str(result)
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
